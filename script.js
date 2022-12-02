2983ae1ad9863cfeb90f5f721ba7d1fa1f03289e
VictorGiuliano <victorgiuliano98@gmail.com>
2022-12-02 16:46:15 +0100

    Stilato punti traccia e iniziato lo script

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log('Js OK');
+//console.log('Js OK');
+/*Chiedi all’utente il suo nome, poi chiedi il suo cognome, poi chiedi il suo colore preferito
+Infine scrivi sulla pagina nomecognomecolorepreferito21*/
+
+/*
+1. Prendo l'elemento dal DOM
+2. Preparo la stringa contenente il numero 12
+3. Chiedere il nome all'utente
+4. Chiedere il cognome all'utente
+5. Chiedi colore preferito
+6. Operazione che unisce le stringhe di tutti i dati che abbiamo ottenuto 
+7. Stampare la password su schermo */
+
+//Prendo elemento dal DOM
+const targetElement = document.getElementById('psw');
+//console.log(targetElement);
+
+
